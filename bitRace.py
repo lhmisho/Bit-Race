d2b9aa003d799f1e35181829005483fefa6e1a56
lhmisho <lhmisho@gmail.com>
2019-11-09 12:49:34 +0600

    moving car around the screen

--- bitRace.py
+++ bitRace.py
@@ -20,17 +20,39 @@ def car(x,y):
 
 x = (display_width * 0.45)
 y = (display_height * 0.8)
+x_change = 0
+y_change = 0
+car_speed = 0
 
 while not crushed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crushed = True
-    
+
+    # determining key press
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
+            elif event.key == pygame.K_UP:
+                y_change = -5
+            elif event.key == pygame.K_DOWN:
+                y_change = 5
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                y_change = 0
+    x += x_change
+    y += y_change
+
     gameDisplay.fill(white)
     car(x,y)
 
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(100)
 
 pygame.quit()
 quit()
