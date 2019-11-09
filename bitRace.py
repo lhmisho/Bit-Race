bc94e5910443ca4be6a30cc334b4763a9a133f61
lhmisho <lhmisho@gmail.com>
2019-11-09 14:00:55 +0600

    adding boundries

--- bitRace.py
+++ bitRace.py
@@ -8,51 +8,66 @@ display_height = 600
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('A bit racing')
 
+red = (255,0,0)
 black = (0, 0, 0)
 white = (255,255,255)
 
+car_width = 73
+
 clock = pygame.time.Clock()
-crushed = False
 carImg = pygame.image.load('racecar.png')
 
 def car(x,y):
     gameDisplay.blit(carImg, (x,y))
 
-x = (display_width * 0.45)
-y = (display_height * 0.8)
-x_change = 0
-y_change = 0
-car_speed = 0
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+    x_change = 0
+    y_change = 0
+    car_speed = 0
+    game_exit = False
 
-while not crushed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crushed = True
+    while not game_exit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_exit = True
 
-    # determining key press
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
-            elif event.key == pygame.K_UP:
-                y_change = -5
-            elif event.key == pygame.K_DOWN:
-                y_change = 5
+        # determining key press
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+                elif event.key == pygame.K_UP:
+                    y_change = -5
+                elif event.key == pygame.K_DOWN:
+                    y_change = 5
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_change = 0
-            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                y_change = 0
-    x += x_change
-    y += y_change
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    y_change = 0
+        x += x_change
+        y += y_change
 
-    gameDisplay.fill(white)
-    car(x,y)
+        gameDisplay.fill(white)
+        car(x,y)
 
-    pygame.display.update()
-    clock.tick(100)
+        if x < 0:
+            x_change = 1
+        if x > display_width - car_width:
+            x_change = -1
+        if y < 0:
+            y_change = 1
+        if y > display_height - car_width:
+            y_change = -1
+            
+        pygame.display.update()
+        clock.tick(100)
 
+
+game_loop()
 pygame.quit()
 quit()
